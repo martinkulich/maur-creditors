f97618096a8837de834c5875946c942487822c4e
Martin Kulich <martin.kulich@imatic.cz>
2012-06-17 22:52:44 +0200

    ...

--- apps/frontend/modules/settlement/templates/_list_td_tabular.php
+++ apps/frontend/modules/settlement/templates/_list_td_tabular.php
@@ -1,3 +1,4 @@
+<?php $currencyCode = $settlement->getContract()->getCurrencyCode()?>
 <?php use_helper('Number') ?>
 <td class="sf_admin_text sf_admin_list_td_creditor no-wrap-line">
     <?php echo $settlement->getCreditor() ?>
@@ -12,29 +13,29 @@
     <?php echo false !== strtotime($settlement->getDate()) ? format_date($settlement->getDate(), "D") : '&nbsp;' ?>
 </td>
 <td class="sf_admin_text sf_admin_list_td_balance ">
-    <?php echo format_currency($settlement->getBalance(), $settlement->getContract()->getCurrencyCode()) ?>
+    <?php echo format_currency($settlement->getBalance(), $currencyCode) ?>
 </td>
 <td class="sf_admin_text sf_admin_list_td_interest ">
-    <?php echo format_currency($settlement->getInterest(), $settlement->getContract()->getCurrencyCode()) ?>
+    <?php echo format_currency($settlement->getInterest(), $currencyCode) ?>
 </td>
 <td class="sf_admin_text sf_admin_list_td_paid ">
-    <?php echo format_currency($settlement->getPaid(), $settlement->getContract()->getCurrencyCode()) ?>
+    <?php echo format_currency($settlement->getPaid(), $currencyCode) ?>
 </td>
 <td class="sf_admin_boolean sf_admin_list_td_cash ">
   <?php echo get_partial('settlement/list_field_boolean', array('value' => $settlement->getCash())) ?>
 </td>
 <td class="sf_admin_text sf_admin_list_td_capitalized ">
-    <?php echo format_currency($settlement->getCapitalized(), $settlement->getContract()->getCurrencyCode()) ?>
+    <?php echo format_currency($settlement->getCapitalized(), $currencyCode) ?>
 </td>
 <td class="sf_admin_text sf_admin_list_td_balance_reduction ">
-    <?php echo format_currency($settlement->getBalanceReduction(), $settlement->getContract()->getCurrencyCode()) ?>
+    <?php echo format_currency($settlement->getBalanceReduction(), $currencyCode) ?>
 </td>
 
 <td class="sf_admin_text sf_admin_list_td_unsettled ">
-        <?php echo format_currency($settlement->getUnsettled(), $settlement->getContract()->getCurrencyCode()) ?>
+        <?php echo format_currency($settlement->getUnsettled(), $currencyCode) ?>
 </td>
 <td class="sf_admin_text sf_admin_list_td_unsettled_cumulative ">
-  <?php echo $settlement->getUnsettledCumulative() ?>
+  <?php echo format_currency($settlement->getUnsettledCumulative(), $currencyCode) ?>
 </td>
 <td class="sf_admin_text sf_admin_list_td_settlement_type ">
         <?php echo $sf_context->getI18n()->__($settlement->getSettlementType()); ?>
