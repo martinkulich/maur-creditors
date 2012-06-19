3abe77420551038cf4c53fe6a6eff3cd1b6e26db
Martin Kulich <martin.kulich@imatic.cz>
2012-06-19 11:30:40 +0200

    format datumu u platby

--- apps/frontend/modules/payment/templates/_list_td_tabular.php
+++ apps/frontend/modules/payment/templates/_list_td_tabular.php
@@ -5,7 +5,7 @@
   <?php echo $payment->getContract() ?>
 </td>
         <td class="sf_admin_date no-wrap-line sf_admin_list_td_date text-align-right">
-  <?php echo false !== strtotime($payment->getDate()) ? format_date($payment->getDate(), "f") : '&nbsp;' ?>
+  <?php echo false !== strtotime($payment->getDate()) ? format_date($payment->getDate(), "D") : '&nbsp;' ?>
 </td>
         <td class="sf_admin_text sf_admin_list_td_amount text-align-right ">
   <?php echo my_format_currency($payment->getAmount(), $payment->getContract()->getCurrencyCode()) ?>
