4259dbbe3a2318dc6386971adab13222786aa521
Martin Kulich <martin.kulich@imatic.cz>
2012-06-19 11:32:27 +0200

    format datumu u platby

--- apps/frontend/modules/payment/templates/_list_th_tabular.php
+++ apps/frontend/modules/payment/templates/_list_th_tabular.php
@@ -9,7 +9,7 @@
 </th>
 <?php end_slot(); ?>
 <?php include_slot('sf_admin.current_header') ?><?php slot('sf_admin.current_header') ?>
-<th class="sf_admin_date sf_admin_list_th_date span2">
+<th class="sf_admin_date sf_admin_list_th_date span1">
   <?php if ('date' == $sort[0]): ?>
     <?php echo link_to(__('Date', array(), 'messages'), '@payment', array('query_string' => 'sort=date&sort_type='.($sort[1] == 'asc' ? 'desc' : 'asc'))) ?>
     <?php echo image_tag(sfConfig::get('root_web_dir').'/images/'.$sort[1].'.png', array('alt' => __($sort[1], array(), 'sf_admin'), 'title' => __($sort[1], array(), 'sf_admin'), 'class'=>'admin_list_sort_img')) ?>
