addea54b746e28bf39d3e68ccb91760d24c71a7c
Martin Kulich <martin.kulich@imatic.cz>
2012-10-04 16:18:55 +0200

    unpaid

--- lib/filter/UnpaidFormFilter.class.php
+++ lib/filter/UnpaidFormFilter.class.php
@@ -56,7 +56,7 @@ class UnpaidFormFilter extends BaseUnpaidFormFilter
     public function doBuildCriteria(array $values)
     {
         $criteria = parent::doBuildCriteria($values);
-        $customCriteria = sprintf("%s = (SELECT MAX(%s) FROM %s WHERE %s <= '%s')", UnpaidPeer::SETTLEMENT_DATE, SettlementPeer::DATE, SettlementPeer::TABLE_NAME, SettlementPeer::DATE, date('Y-m-d'));
+        $customCriteria = sprintf("%s = (SELECT MAX(s1.date) FROM settlement s1 WHERE s1.date <= '%s' AND s1.contract_id = %s)", UnpaidPeer::SETTLEMENT_DATE, date('Y-m-d'), UnpaidPeer::CONTRACT_ID);
         $criteria->add(UnpaidPeer::SETTLEMENT_DATE, $customCriteria, Criteria::CUSTOM);
         return $criteria;
     }
