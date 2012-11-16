c40952dde6d74868994bebc045e6c5c8f2d6cd23
martinkulich <martinkulich@10.1.20.105>
2012-11-16 09:48:36 +0100

    account form

--- plugins/securityPlugin/modules/security/templates/accountSuccess.php
+++ plugins/securityPlugin/modules/security/templates/accountSuccess.php
@@ -1,6 +1,6 @@
 <div class="row">
     <div class="span3 offset4">
-        <div class="span-login-form well container">
+        <div class="span-login-form well">
             <form action="<?php echo url_for('@account') ?>" method="post">
                 <?php include_partial('default/form', array('form' => $form)) ?>
                 <input type="submit" class="btn btn-primary btn-account pull-right" value="<?php echo __('Update') ?>" />
