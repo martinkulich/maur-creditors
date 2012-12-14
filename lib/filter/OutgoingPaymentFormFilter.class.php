600da8c4232440307ca7b166a7afd2d51dcba3ab
martinkulich <martinkulich@10.1.20.105>
2012-12-14 15:04:20 +0100

    vyplaty na rok dopredu

--- lib/filter/OutgoingPaymentFormFilter.class.php
+++ lib/filter/OutgoingPaymentFormFilter.class.php
@@ -13,7 +13,7 @@ class OutgoingPaymentFormFilter extends BaseOutgoingPaymentFormFilter
     public function configure()
     {
         $this->setWidget('date', new myJQueryDateWidget());
-        $this->setValidator('date', new myValidatorDate());
+        $this->setValidator('date', new myValidatorDate(array('required'=>false)));
         
     }
 
