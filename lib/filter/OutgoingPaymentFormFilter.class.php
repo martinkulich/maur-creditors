c768e780c8afd38763e5458eac663848d67503c8
Martin Kulich <martin.kulich@imatic.cz>
2013-01-11 18:23:06 +0100

    filtr na datum odchozi platby

--- lib/filter/OutgoingPaymentFormFilter.class.php
+++ lib/filter/OutgoingPaymentFormFilter.class.php
@@ -12,9 +12,10 @@ class OutgoingPaymentFormFilter extends BaseOutgoingPaymentFormFilter
 
     public function configure()
     {
-        $this->setWidget('date', new myJQueryDateWidget());
-        $this->setValidator('date', new myValidatorDate(array('required'=>false)));
-        
+        $this->setWidget('date', new MyJQueryFormFilterDate());
+
+        $this->setValidator('date', new MyValidatorDateRange(array('required' => false)));
+
         $this->getWidget('creditor_id')->setOption('order_by', array('Lastname', 'asc'));
         
     }
