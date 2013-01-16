f3a476ee7f2e6cbb2df57ba005acddaade2d4f69
Martin Kulich <martin.kulich@imatic.cz>
2013-01-16 20:27:31 +0100

    darky

--- apps/frontend/modules/settlement/templates/newOutgoingPaymentSuccess.php
+++ apps/frontend/modules/settlement/templates/newOutgoingPaymentSuccess.php
@@ -2,7 +2,7 @@
 <div class="modal_content">
     <div class="modal-header" >
         <a class="close" data-dismiss="modal">×</a>
-        <h3><?php echo $settlement->getContract()->getCreditor(). ' - '.$settlement->getContract() . ' - '. __('New outgoingPayment') ?></h3>
+        <h3><?php echo $settlement->getContract()->getCreditor() . ' - '. __('New outgoingPayment') ?></h3>
     </div>
     <?php include_partial('settlement/new_outgoing_payment_form', array('settlement' => $settlement, 'form' => $form, 'configuration' => $configuration, 'helper' => $helper)) ?>
 </div>
