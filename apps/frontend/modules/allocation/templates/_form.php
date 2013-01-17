8db8c668b6a27484cc532fd3d626987158b9cae3
Martin Kulich <martin.kulich@imatic.cz>
2013-01-17 10:39:10 +0100

    darky

--- apps/frontend/modules/allocation/templates/_form.php
+++ apps/frontend/modules/allocation/templates/_form.php
@@ -1,20 +1,23 @@
 <?php use_stylesheets_for_form($form) ?>
 <?php use_javascripts_for_form($form) ?>
 
-<form action="<?php echo  url_for('@settlement_allocate?id='.$settlement->getId()) ?>" method="post" class="form form-horizontal form-modal" name="formMask">
+<?php echo form_tag_for($form, '@allocation', array('class'=>'form form-horizontal form-modal', 'name'=>'formMask')) ?>
 <div class="modal-body">
     <?php include_component('default','flashes') ?>
     <?php echo $form->renderHiddenFields(false) ?>
     <?php foreach ($form as $key=>$field): ?>
         <?php if(!$field->isHidden()){ ?>
-          <?php include_partial('settlement/form_field_horizontal', array( 'form' => $form, 'key' => $key)) ?>
+          <?php include_partial('allocation/form_field_horizontal', array( 'form' => $form, 'key' => $key)) ?>
         <?php } ?>
     <?php endforeach; ?>
 
 </div>
 <div class="modal-footer">
-    <?php include_partial('settlement/form_actions', array('settlement' => $settlement, 'form' => $form, 'configuration' => $configuration, 'helper' => $helper)) ?>
+    <?php include_partial('allocation/form_actions', array('allocation' => $allocation, 'form' => $form, 'configuration' => $configuration, 'helper' => $helper)) ?>
 </div>
 </form>
-
-
+<script type="text/javascript">
+    $(document).ready(function(){
+        jQuery('#allocation_creditor_id').change();
+    });
+</script>
--- lib/model/Allocation.php
+++ lib/model/Allocation.php
@@ -6,7 +6,7 @@ require_once 'lib/model/om/BaseAllocation.php';
 /**
  * Skeleton subclass for representing a row from the 'allocation' table.
  *
- * 
+ *
  *
  * You should add additional methods to this class to meet the
  * application requirements.  This class will only be generated as
@@ -14,25 +14,30 @@ require_once 'lib/model/om/BaseAllocation.php';
  *
  * @package    lib.model
  */
-class Allocation extends BaseAllocation {
+class Allocation extends BaseAllocation
+{
 
-	/**
-	 * Initializes internal state of Allocation object.
-	 * @see        parent::__construct()
-	 */
-	public function __construct()
-	{
-		// Make sure that parent constructor is always invoked, since that
-		// is where any default values for this object are set.
-		parent::__construct();
-	}
+    /**
+     * Initializes internal state of Allocation object.
+     * @see        parent::__construct()
+     */
+    public function __construct()
+    {
+        // Make sure that parent constructor is always invoked, since that
+        // is where any default values for this object are set.
+        parent::__construct();
+    }
 
     /**
      * @return Contract
      */
     public function getContract()
     {
-        return $this->getSettlement()->getContract();
+        $contract = null;
+        if ($this->getSettlement()) {
+            $contract = $this->getSettlement()->getContract();
+        }
+        return $contract;
     }
 
     /**
@@ -40,7 +45,11 @@ class Allocation extends BaseAllocation {
      */
     public function getCreditor()
     {
-        return $this->getContract()->getCreditor();
+        $creditor = null;
+        if ($this->getContract()) {
+            $creditor = $this->getContract()->getCreditor();
+        }
+        return $creditor;
     }
 
     public function getAllocated()
