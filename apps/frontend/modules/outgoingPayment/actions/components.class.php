f1fc03ca054860101d793af7cbc8aff521fe52ca
Martin Kulich <martin.kulich@imatic.cz>
2013-01-17 14:43:35 +0100

    darky

--- apps/frontend/modules/outgoingPayment/actions/components.class.php
+++ apps/frontend/modules/outgoingPayment/actions/components.class.php
@@ -12,7 +12,23 @@ class outgoingPaymentComponents extends sfComponents
         $criteria->add(OutgoingPaymentPeer::CREDITOR_ID, $request->getParameter('creditor_id'));
 
         $this->outgoingPaymentId = $request->getParameter('default', 0);
-        $this->widget = new sfWidgetFormPropelChoice(array('model' => 'OutgoingPayment', 'criteria' => $criteria, 'add_empty' => true));
+        $formClass = sfInflector::humanize($this->formName . 'Form');
+        if (class_exists($formClass)) {
+            if ($this->formName == 'allocation') {
+                $creditor = CreditorPeer::retrieveByPK($request->getParameter('creditor_id'));
+                $outgoingPayment = OutgoingPaymentPeer::retrieveByPK($this->outgoingPaymentId);
+                $criteria = ServiceContainer::getAllocationService()->getAllocableOutgoingPaymentsCriteria($creditor, $outgoingPayment);
+
+            }
+            $form = new $formClass;
+
+
+            $this->widget = $form->getWidget('outgoing_payment_id');
+        } else {
+            $this->widget = new sfWidgetFormPropelChoice(array('model' => 'OutgoingPayment', 'criteria' => $criteria, 'add_empty' => true));
+        }
+
+        $this->widget->setOption('criteria', $criteria);
 
     }
 }
